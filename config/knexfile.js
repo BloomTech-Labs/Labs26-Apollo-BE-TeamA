2bae5d822bc9ccc38284aad5fcbb672941d0587a
Cody <61713646+codemannnnnn@users.noreply.github.com>
2020-09-10 15:33:36 -0600

    Update knexfile.js

--- config/knexfile.js
+++ config/knexfile.js
@@ -25,5 +25,9 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
+        pool: {
+      min: 2,
+      max: 10,
+    },
   },
 };
