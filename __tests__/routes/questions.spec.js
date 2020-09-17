9940eef6df6e21dfd1e0a2de1fb6d3b14c3c2fbf
codemannnnnn <codybranson@gmail.com>
2020-09-17 10:23:20 -0600

    added testing to questions and response files. next is topic and thread

--- __tests__/routes/questions.spec.js
+++ __tests__/routes/questions.spec.js
@@ -4,9 +4,46 @@ const server = require("../../api/app.js");
 describe("Can retrieve info on questions", () => {
   it("can pull question data", () => {
     return supertest(server)
-      .get("/question")
+      .get(
+        "/question"
+        //  {
+        //   headers: { Authorization: `Bearer ${process.env.TOKENID}` },
+        // }
+      )
       .then((res) => {
         expect(res.status).toBe(200);
+      });
+  });
+});
+
+describe("GET requests for endpoint /question/:id", () => {
+  it("Should return question ID 1 object info", () => {
+    return supertest(server)
+      .get("/question/1")
+      .then((res) => {
+        expect(res.body).toMatchObject({
+          id: 1,
+          type: "Context Questions",
+          style: "Text",
+          question: "What is your current priority?",
+        });
+      });
+  });
+
+  it("Should return the error message for a non valid id", () => {
+    return supertest(server)
+      .get("/question/10")
+      .then((res) => {
+        expect(res.body).toMatchObject({
+          error: "QuestionNotFound",
+        });
+      });
+  });
+  it("Should return status code 404 for id not found", () => {
+    return supertest(server)
+      .get("/question/10")
+      .then((res) => {
+        expect(res.status).toBe(404);
       });
   });
 });
--- __tests__/routes/response.spec.js
+++ __tests__/routes/response.spec.js
@@ -10,3 +10,30 @@ describe("Can retrieve info on questions", () => {
       });
   });
 });
+
+describe("GET Can retrieve data on responses", () => {
+  it("Can return status code of 200", () => {
+    return supertest(server)
+      .get("/response")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("Can retrieve object from response endpoint", () => {
+    return supertest(server)
+      .get("/response")
+      .then((res) => {
+        expect(res.body).toMatchObject([
+          {
+            id: 1,
+            questionid: 1,
+            response: "This is my response.",
+            respondedby: "00ulthapbErVUwVJy4x6",
+            topicid: 1,
+            created_at: "2020-09-15T16:27:11.031Z",
+            updated_at: "2020-09-15T16:27:11.031Z",
+          },
+        ]);
+      });
+  });
+});
