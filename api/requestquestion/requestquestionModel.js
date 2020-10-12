800a50a9564dba8fdff155025bf53603c280390d
codemannnnnn <codybranson@gmail.com>
2020-10-12 10:43:12 -0600

    new fixes

--- api/requestquestion/requestquestionModel.js
+++ api/requestquestion/requestquestionModel.js
@@ -1,32 +1,37 @@
-const db = require('../../data/db-config');
+const db = require("../../data/db-config");
 
 const findAll = async () => {
-  return await db('survey_request_questions');
+  return await db("survey_request_questions");
 };
 
 const findBy = (filter) => {
-  return db('survey_request_questions').where(filter);
+  return db("survey_request_questions").where(filter);
 };
 
 const findById = async (id) => {
-  return db('survey_request_questions').where({ surveyrequestid: id }).first().select('*');
+  return db("survey_request_questions")
+    .where({ surveyrequestid: id })
+    .first()
+    .select("*");
 };
 
 const create = async (question) => {
-  return db('survey_request_questions').insert(question).returning('*');
+  return db("survey_request_questions").insert(question).returning("*");
 };
 
 const update = (id, question) => {
   console.log(question);
-  return db('survey_request_questions')
+  return db("survey_request_questions")
     .where({ surveyrequestid: id })
     .first()
     .update(question)
-    .returning('*');
+    .returning("*");
 };
 
 const remove = async (id) => {
-  return await db('survey_request_questions').where({ surveyrequestid: id }).del();
+  return await db("survey_request_questions")
+    .where({ surveyrequestid: id })
+    .del();
 };
 
 module.exports = {
